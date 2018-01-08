af327c341e0ca9ed1683de1d36f6cd343694b2f9
chcoq <ch_coq>
2018-01-08 23:24:01 +0100

    Activation et désactivation du bouton jouer si le joueur à saisi un caractère dans le champs de saisi

--- app/src/main/java/quiz/top/fr/topquiz/MainActivity.java
+++ app/src/main/java/quiz/top/fr/topquiz/MainActivity.java
@@ -2,10 +2,13 @@ package quiz.top.fr.topquiz;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.text.Editable;
+        import android.text.TextWatcher;
         import android.widget.Button;
         import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //Déclaration des Instances
     private EditText editTextNom;
     private Button buttonJouer;
 
@@ -16,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
         editTextNom = findViewById(R.id.editTextNom);
         buttonJouer = findViewById(R.id.buttonJouer);
+
+        //désactivation du bouton jouer
+        buttonJouer.setEnabled(false);
+        //On vérifie si on entre du text dans le champ Nom
+        editTextNom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                buttonJouer.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+        });
+
     }
+
 }
